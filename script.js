2eaa5f3de3ab7e24a5a65ad87956112b3416b730
Sintiya <124550908+sintiyaaa73@users.noreply.github.com>
2023-11-29 14:53:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-//Toggle class active
+//Toggle class active untuk menu
 const navbarnav = document.querySelector(".navbar-nav");
 //ketika menu di klik
 document.querySelector("#menu").onclick = () => {
   navbarnav.classList.toggle("active");
+};
+
+//Toogle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelecor('#search-button').onclick = () => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
 };
 
 //klik diluar syber untuk menghilangkan nav
